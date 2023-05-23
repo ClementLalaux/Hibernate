fa042ae6c6dd9e19c529548acdd9530d9d47ac61
Clement Lalaux <lalauxclement@gmail.com>
2023-05-23 16:41:58 +0200

    Hibernate exercice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,5 @@
 package org.example;
 
-import org.example.entities.Produit;
-import org.example.services.ProduitService;
-
-import java.util.Date;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
